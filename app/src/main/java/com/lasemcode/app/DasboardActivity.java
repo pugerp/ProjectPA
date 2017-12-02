6c258b4d587749e2a892d15c2b64e82f08fdfbbe
pugerp <33994455+pugerp@users.noreply.github.com>
2017-12-02 18:29:59 +0700

    test

--- app/src/main/java/com/lasemcode/app/DasboardActivity.java
+++ app/src/main/java/com/lasemcode/app/DasboardActivity.java
@@ -1,5 +1,6 @@
 package com.lasemcode.app;
 
+import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
@@ -10,14 +11,38 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class DasboardActivity extends AppCompatActivity {
     public static final String KEYPREF     = "LocalData";
+    int PLACE_PICKER_REQUEST = 1;
+    Button btnLocation;
+    EditText alamat, latitude;
+    WebView attributionText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dasboard);
 
+        btnLocation = (Button) findViewById(R.id.location);
+        alamat = (EditText) findViewById(R.id.editAlamat);
+        latitude = (EditText) findViewById(R.id.editLatitude);
+        attributionText = (WebView) findViewById(R.id.webView);
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLocation();
+                Log.i("test","cek picker");
+            }
+        });
 
 
     }
@@ -49,4 +74,44 @@ public class DasboardActivity extends AppCompatActivity {
 
     }
 
+    public void getLocation(){
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        Intent intent;
+        try {
+            intent = builder.build(this);
+            startActivityForResult(intent, PLACE_PICKER_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK){
+//            if(requestCode == RESULT_OK){
+//                Place place = PlacePicker.getPlace(DasboardActivity.this, data);
+//                String address = String.valueOf(place.getAddress());
+//                String lat = String.valueOf(place.getLatLng());
+//                alamat.setText(address);
+//                latitude.setText(lat);
+//                if(place.getAttributions() == null){
+//                    attributionText.loadData("no attribution", "text/html; charset=utf-8", "UFT-8");
+//                }else{
+//                    attributionText.loadData(place.getAttributions().toString(), "text/html; charset=utf-8", "UFT-8");
+//                }
+//
+//            }
+            displayPlacePicker(data);
+        }
+    }
+
+    private void displayPlacePicker(Intent data){
+        Place place = PlacePicker.getPlace(data, this);
+
+        String address = String.valueOf(place.getAddress());
+        String lat = String.valueOf(place.getLatLng());
+        alamat.setText(address);
+        latitude.setText(lat);
+
+    }
 }
