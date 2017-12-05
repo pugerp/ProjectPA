7858dee482d36a75a574dc2c26e49418a43e4861
pugerp <33994455+pugerp@users.noreply.github.com>
2017-12-05 15:40:43 +0700

    Kamera & Place Picker

--- app/src/main/java/com/lasemcode/app/DasboardActivity.java
+++ app/src/main/java/com/lasemcode/app/DasboardActivity.java
@@ -3,8 +3,14 @@ package com.lasemcode.app;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,27 +26,41 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.File;
 
 public class DasboardActivity extends AppCompatActivity {
     public static final String KEYPREF     = "LocalData";
-    int PLACE_PICKER_REQUEST = 1;
-    Button btnLocation;
-    EditText alamat, latitude;
+    int PLACE_PICKER_REQUEST = 1, REQUEST_CAMERA = 2, SELECT_FILE = 0;
+    Button btnLocation, btnPhoto;
+    EditText alamat, latitude, photoPath, longtitude;
     WebView attributionText;
+    Uri UrlGambar;
+    File file;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dasboard);
 
         btnLocation = (Button) findViewById(R.id.location);
+        btnPhoto = (Button) findViewById(R.id.btnSelectPhoto);
         alamat = (EditText) findViewById(R.id.editAlamat);
+        photoPath = (EditText) findViewById(R.id.editPhoto);
         latitude = (EditText) findViewById(R.id.editLatitude);
-        attributionText = (WebView) findViewById(R.id.webView);
+        longtitude = (EditText) findViewById(R.id.editLongitude);
         btnLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getLocation();
                 Log.i("test","cek picker");
+            }
+        });
+
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectPhoto();
             }
         });
 
@@ -85,33 +105,74 @@ public class DasboardActivity extends AppCompatActivity {
         }
     }
 
+    private void selectPhoto(){
+        final CharSequence[] items = {"Camera","Gallery","Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(DasboardActivity.this);
+        builder.setTitle("Tambahkan Photo");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(items[i].equals("Camera")){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    file = new File(Environment.getExternalStorageDirectory(), "Foto Jalan Rusak/img_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    UrlGambar = Uri.fromFile(file);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, UrlGambar);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                }else if(items[i].equals("Gallery")){
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/");
+                    startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
+                }else{
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String path ="";
         if(requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK){
-//            if(requestCode == RESULT_OK){
-//                Place place = PlacePicker.getPlace(DasboardActivity.this, data);
-//                String address = String.valueOf(place.getAddress());
-//                String lat = String.valueOf(place.getLatLng());
-//                alamat.setText(address);
-//                latitude.setText(lat);
-//                if(place.getAttributions() == null){
-//                    attributionText.loadData("no attribution", "text/html; charset=utf-8", "UFT-8");
-//                }else{
-//                    attributionText.loadData(place.getAttributions().toString(), "text/html; charset=utf-8", "UFT-8");
-//                }
-//
-//            }
             displayPlacePicker(data);
         }
+
+        if(resultCode == DasboardActivity.RESULT_OK){
+            if(requestCode == REQUEST_CAMERA){
+                path = file.getPath();
+            }else if(requestCode == SELECT_FILE){
+                UrlGambar = data.getData();
+                path = getRealPath(UrlGambar);
+                if(path == null){
+                    path = UrlGambar.getPath();
+                }
+            }
+        }
+        photoPath.setText(path);
     }
 
     private void displayPlacePicker(Intent data){
         Place place = PlacePicker.getPlace(data, this);
 
         String address = String.valueOf(place.getAddress());
-        String lat = String.valueOf(place.getLatLng());
+        LatLng lat = place.getLatLng();
         alamat.setText(address);
-        latitude.setText(lat);
+        latitude.setText(String.valueOf(lat.latitude));
+        longtitude.setText(String.valueOf(lat.longitude));
 
+
+    }
+
+    private String getRealPath(Uri contentUri) {
+        String path = null;
+        String[] img_data = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, img_data, null, null, null);
+        if (cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            path = cursor.getString(column_index);
+        }
+        cursor.close();
+        return path;
     }
 }
---
+++ app/src/main/java/com/lasemcode/app/Database.java
@@ -0,0 +1,35 @@
+package com.lasemcode.app;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Z & N on 04/12/2017.
+ */
+
+public class Database extends SQLiteOpenHelper {
+    public static String DB_NAME = "dbpengaduan";
+    public static int DB_VERSION = 1;
+    public Database(Context context){
+        super(context,DB_NAME,null,DB_VERSION);
+    }
+    public Database(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public Database(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTablePengaduan = "CREATE TABLE pengaduan(" + "idPengaduan INTEGER PRIMARY KEY, " + "idUser INTEGER, " + ");";
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
